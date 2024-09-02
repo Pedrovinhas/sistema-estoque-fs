fec2b5b9c9c08620b4dcb8146193821425080094
Pedro Henrique Vinhas <pedrohenrique.hrzon@gmail.com>
2024-09-02 17:59:30 -0300

    feat(): add pedido and produto services.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -5,14 +5,20 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use App\Contracts\Services\CategoriaServiceContract;
 use App\Contracts\Services\EstabelecimentoServiceContract;
+use App\Contracts\Services\PedidoServiceContract;
+use App\Contracts\Services\ProdutoServiceContract;
 use App\Services\CategoriaService;
 use App\Services\EstabelecimentoService;
+use App\Services\PedidoService;
+use App\Services\ProdutoService;
 
 class AppServiceProvider extends ServiceProvider
 {
   public $singletons = [
     CategoriaServiceContract::class => CategoriaService::class,
     EstabelecimentoServiceContract::class => EstabelecimentoService::class,
+    ProdutoServiceContract::class => ProdutoService::class,
+    PedidoServiceContract::class => PedidoService::class,
   ];
   /**
    * Register any application services.
