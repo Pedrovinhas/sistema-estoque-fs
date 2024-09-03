4fed942482746445af4fff467054923d0d25f75c
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2024-09-02 21:14:05 -0300

    refactor(): change some routes to follow rest.

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CategoriaController;
 use App\Http\Controllers\EstabelecimentoController;
 use App\Http\Controllers\PedidoController;
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
@@ -12,7 +13,7 @@ Route::middleware('auth')->group(function () {
       Route::get('/', [CategoriaController::class, 'listCategoriaProduto']);
       Route::post('/', [CategoriaController::class, 'createCategoriaProduto']);
     });
-  
+
     Route::prefix('estabelecimento')->group(function () {
       Route::get('/', [CategoriaController::class, 'listCategoriaEstabelecimento']);
       Route::post('/', [CategoriaController::class, 'createCategoriaEstabelecimento']);
@@ -24,7 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::post('/', [EstabelecimentoController::class, 'create']);
 
     Route::prefix('/{estabelecimentoId}')->group(function () {
-      Route::get('/pedidos', [EstabelecimentoController::class, 'listPedidos']);
+      Route::get('/pedidos', [PedidoController::class, 'listPedidosByEstabelecimento']);
     });
   });
 
@@ -33,7 +34,13 @@ Route::middleware('auth')->group(function () {
     Route::post('/', [ProdutoController::class, 'create']);
   });
 
+  Route::prefix('users/{userId}')->group(function () {
+    Route::get('/', [UserController::class, 'getUser']);
+    Route::get('/pedidos', [PedidoController::class, 'listPedidosByUser']);
+  });
+
   Route::prefix('pedidos')->group(function () {
+    Route::get('/', [PedidoController::class, 'listWithFilters']);
     Route::post('/', [PedidoController::class, 'create']);
   });
 });
