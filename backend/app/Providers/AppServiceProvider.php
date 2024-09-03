ad8d8609f7e641c06a2ee6248f03e60b1b68174f
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2024-09-02 21:13:07 -0300

    feat(): add new services in provider.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -7,10 +7,12 @@ use App\Contracts\Services\CategoriaServiceContract;
 use App\Contracts\Services\EstabelecimentoServiceContract;
 use App\Contracts\Services\PedidoServiceContract;
 use App\Contracts\Services\ProdutoServiceContract;
+use App\Contracts\Services\UserServiceContract;
 use App\Services\CategoriaService;
 use App\Services\EstabelecimentoService;
 use App\Services\PedidoService;
 use App\Services\ProdutoService;
+use App\Services\UserService;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     EstabelecimentoServiceContract::class => EstabelecimentoService::class,
     ProdutoServiceContract::class => ProdutoService::class,
     PedidoServiceContract::class => PedidoService::class,
+    UserServiceContract::class => UserService::class,
   ];
   /**
    * Register any application services.
