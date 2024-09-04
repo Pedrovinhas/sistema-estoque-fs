34e8d4663e1e3e10d679de838d68278bd47c9a5e
Pedro Henrique Vinhas <peuvinhas2@gmail.com>
2024-09-04 01:12:12 -0300

    feat(): add insufficient balance exception.

--- backend/core/Exceptions/InsufficientBalanceException.php
+++ backend/core/Exceptions/InsufficientBalanceException.php
@@ -6,13 +6,28 @@ use Exception;
 
 class InsufficientBalanceException extends Exception
 {
+    protected float $requestedAmount;
+    protected float $currentBalance;
+
     public function __construct(
         float $requestedAmount,
         float $currentBalance,
         int $code = 0,
         \Throwable $previous = null
     ) {
+        $this->requestedAmount = $requestedAmount;
+        $this->currentBalance = $currentBalance;
         $message = "Saldo insuficiente. Saldo do produto: $requestedAmount, Saldo atual: $currentBalance.";
         parent::__construct($message, $code, $previous);
+    }
+
+    public function getRequestedAmount(): float
+    {
+        return $this->requestedAmount;
+    }
+
+    public function getCurrentBalance(): float
+    {
+        return $this->currentBalance;
     }
 }
